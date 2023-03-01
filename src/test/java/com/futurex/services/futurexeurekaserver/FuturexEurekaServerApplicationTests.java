e0f8b0ce0d22727d8b57bd57e48d610b2f27300b
Simon Vargas <sjvargas01@gmail.com>
2023-02-28 23:19:18 -0500

    first commit

---
+++ src/test/java/com/futurex/services/futurexeurekaserver/FuturexEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurex.services.futurexeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturexEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
